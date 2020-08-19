1d9da526b5dd648f95c0ca51c4519128663780d0
Cryptik <cryptik@localhost.localdomain>
2020-08-19 19:35:11 +0530

    version 3

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World! Version is 2"
+    return "Hello World! Version is 3"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5555)
