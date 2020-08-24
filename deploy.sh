ea0a97b18dbfb825fab9cb5cbc7a091d86b7c4fb
Cryptik <cryptik@www.ex2.com>
2020-08-24 20:42:40 +0530

    Production deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 export DOCKER_HOST=tcp://54.218.101.20:2375
 export DOCKER_TLS_VERIFY=
 
-if [ "$(docker ps -a | grep flsktest)" ]
+if [ "$(docker ps -a | grep flskapp)" ]
 then
     docker rm -f flsktest
 fi
-
-docker run -d -p 5555:5555 --name flsktest sw9719/circleciflask:latest
+docker pull sw9719/circleciflask:latest
+docker run -d -p 5556:5556 --name flskapp sw9719/circleciflask:latest
 
